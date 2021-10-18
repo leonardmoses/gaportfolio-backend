36743a1578bd02ca3774bc7105beb5da0b9c014d
leonardmoses <leonard.moses@anemote.com>
2021-10-18 10:55:03 -0400

    backend complete

---
+++ server.js
@@ -0,0 +1,47 @@
+// =======================================
+//              DEPENDENCIES
+// =======================================
+const express = require("express");
+const cors = require("cors");
+// =======================================
+//              IMPORT JSON
+// =======================================
+const projects = require("./projects.json");
+const about = require("./about.json");
+// =======================================
+//              APP OBJECT
+// =======================================
+const app = express();
+// =======================================
+//              MIDDLEWARE
+// =======================================
+app.use(cors());
+// =======================================
+//              ROUTES
+// =======================================
+// INDEX (get)
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/projects", (req, res) => {
+    // send projects via JSON
+    res.json(projects);
+});
+
+app.get("/about", (req, res) => {
+    // send projects via JSON
+    res.json(about);
+});
+
+// NEW (get)
+// DESTROY (delete)
+// UPDATE (put)
+// CREATE (post)
+//EDIT (get) (put)
+// SHOW (get)
+// =======================================
+//              LISTENER
+// =======================================
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
